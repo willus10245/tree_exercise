1449e60879b870c271b1ff5b704bde7815979001
Scott Wiggins <dscottwiggins@gmail.com>
2018-05-07 18:11:57 -0400

    all tests passing

---
+++ src/main/kotlin/BinaryTree.kt
@@ -0,0 +1,29 @@
+class BinaryTree(rootValue: Int, private val rootNode: Node = Node(rootValue)) {
+
+    fun insert(newValue: Int, node: Node = rootNode) {
+        when {
+            newValue < node.value && node.left == null  -> node.left = Node(newValue)
+            newValue < node.value && node.left != null  -> insert(newValue, node.left!!)
+            newValue > node.value && node.right == null -> node.right = Node(newValue)
+            newValue > node.value && node.right != null -> insert(newValue, node.right!!)
+            else -> throw IllegalArgumentException("A node with that value already exists!")
+        }
+    }
+
+    fun depthFirstSearch(searchValue: Int) = inOrderTraversal().contains(searchValue)
+
+    fun inOrderTraversal(node: Node = rootNode, currentList: MutableList<Int> = mutableListOf()): List<Int> {
+        if (node.left != null) inOrderTraversal(node.left!!, currentList)
+
+        currentList.add(node.value)
+
+        if (node.right != null) inOrderTraversal(node.right!!, currentList)
+
+        return currentList
+    }
+}
+
+data class Node(val value: Int) {
+    var left: Node? = null
+    var right: Node? = null
+}
